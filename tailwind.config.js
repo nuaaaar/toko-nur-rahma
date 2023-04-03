c3537f941a246387c86b7c3a6c49e05f6b83d39e
Yanuar Fabien <yanuar.fabien.yf@gmail.com>
2023-04-03 11:22:12 +0800

    style #1: change base color and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+    important: true,
 	content: [
 		"./resources/**/*.blade.php",
 		"./resources/**/*.js",
 		"./resources/**/*.vue",
-        "./node_modules/flowbite/**/*.js"
+        "./node_modules/flowbite/**/*.js",
 	],
-  theme: {
-    extend: {
-        container: {
-            center: true,
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.orange,
+                secondary: colors.gray,
+            },
+            container: {
+                center: true,
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
+            }
         },
     },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
 
