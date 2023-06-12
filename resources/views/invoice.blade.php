273061f457702b3a69bc383d0c3fae415f599f6d
risu331 <sealdragon331@gmail.com>
2023-06-12 19:42:25 +0700

    invoice

---
+++ resources/views/invoice.blade.php
@@ -0,0 +1,233 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Invoice </title>
+<meta name="robots" content="noindex,nofollow" />
+<meta name="viewport" content="width=device-width; initial-scale=1.0;" />
+<style type="text/css">
+  @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700);
+
+  body {
+    background: rgb(204, 204, 204);
+  }
+
+  page {
+    background: white;
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 0.5cm;
+    box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+  }
+
+  page[size="A4"] {
+    width: 21cm;
+    height: 29.7cm;
+  }
+
+  page[size="A4"][layout="portrait"] {
+    width: 29.7cm;
+    height: 21cm;
+  }
+
+  @media print {
+
+    body,
+    page {
+      margin: 0;
+      box-shadow: 0;
+    }
+  }
+</style>
+
+<page size="A4">
+    <div style="padding: 5px;">
+        <table width="100%">
+            <tr>
+                <td>
+                    <strong style="font-size: 24px; color: red;">TAKATIKITUKU</strong>
+                    <br>
+                    &nbsp;
+                </td>
+                <td align="right">
+                    <b>INVOICE</b>
+                    <br>
+                    <small style="color: red;">INV/20230603/MPL/3269414873</small>
+                </td>
+            </tr>
+        </table>
+        <div style="display: flex">
+            <table width="50%">
+                <tr style="font-size: 14px;">
+                    <td colspan="2">
+                        <strong>DITERBITKAN ATAS NAMA</strong>
+                    </td>
+                </tr>
+                <tr style="font-size: 12px;">
+                    <td width="20%">
+                        Penjual
+                    </td>
+                    <td>
+                        : <strong>NR AQUATICS</strong>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" height="100%"></td>
+                </tr>
+            </table>
+            <table width="50%">
+                <tr style="font-size: 14px;">
+                    <td colspan="3">
+                        <strong>UNTUK</strong>
+                    </td>
+                </tr>
+                <tr style="font-size: 12px;">
+                    <td width="30%" style="vertical-align: top;">
+                        Pembeli
+                    </td>
+                    <td width="2%" style="vertical-align: top;">
+                        :
+                    </td>
+                    <td style="vertical-align: top;">
+                        <strong>EDI</strong>
+                    </td>
+                </tr>
+                <tr style="font-size: 12px;">
+                    <td width="30%" style="vertical-align: top;">
+                        Tanggal Pembelian
+                    </td>
+                    <td width="2%" style="vertical-align: top;">
+                        :
+                    </td>
+                    <td style="vertical-align: top;">
+                        <strong>03 Juni 2023</strong>
+                    </td>
+                </tr>
+                <tr style="font-size: 12px;">
+                    <td width="30%" style="vertical-align: top;">
+                        Alamat Pengiriman
+                    </td>
+                    <td width="2%" style="vertical-align: top;">
+                        :
+                    </td>
+                    <td style="vertical-align: top;">
+                        <strong>Ed* Suha*** (*********0842) Ed* Suha*** (*********0842) Ed* Suha*** (*********0842) Ed* Suha*** (*********0842)</strong>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <table width="100%" style="margin-top: 20px;">
+            <tr>
+                <td height="1" style="border-top: 2px solid black;" colspan="4"></td>
+            </tr>
+            <tr style="font-size: 12px;">
+                <td width="50%" style="padding: 10px;">
+                    <strong>INFO PRODUK</strong>
+                </td>
+                <td width="10%" align="right">
+                    <strong>JUMLAH</strong>
+                </td>
+                <td width="20%" align="right">
+                    <strong>HARGA SATUAN</strong>
+                </td>
+                <td width="20%" align="right">
+                    <strong>TOTAL HARGA</strong>
+                </td>
+            </tr>
+            <tr>
+                <td height="1" style="border-bottom: 2px solid black;" colspan="4"></td>
+            </tr>
+            <tr style="font-size: 12px;">
+                <td width="50%" style="padding: 10px;">
+                    <strong style="color: red; font-size: 14px;">New Life Spectrum NLS Thera A 280g small</strong>
+                    <br>
+                    <small>Berat: 400 gr</small>
+                </td>
+                <td width="10%" align="right">
+                    1
+                </td>
+                <td width="20%" align="right">
+                    Rp.309.000
+                </td>
+                <td width="20%" align="right">
+                    Rp.309.000
+                </td>
+            </tr>
+            <tr>
+                <td height="1" style="border-bottom: 0.5px solid gray;" colspan="4"></td>
+            </tr>
+            <tr>
+                <td height="15" colspan="4"></td>
+            </tr>
+            <tr style="font-size: 12px;">
+                <td width="50%" height="10"></td>
+                <td width="20%" colspan="2" align="left">
+                    <strong>TOTAL HARGA (1 BARANG)</strong>
+                </td>
+                <td width="20%" align="right">
+                    Rp.309.000
+                </td>
+            </tr>
+            <tr style="font-size: 12px;">
+                <td width="50%"></td>
+                <td width="20%" colspan="2" align="left">
+                    Total Ongkos Kirim (400 gr)
+                </td>
+                <td width="20%" align="right">
+                    Rp.9.000
+                </td>
+            </tr>
+            <tr style="font-size: 12px;">
+                <td width="50%"></td>
+                <td width="20%" colspan="2" align="left">
+                    Ongkos Kirim diteruskan ke Kurir
+                </td>
+                <td width="20%" align="right">
+                    - Rp.9.000
+                </td>
+            </tr>
+            <tr style="font-size: 12px;">
+                <td width="50%" height="10"></td>
+                <td width="50%" colspan="3" height="10" style="border-bottom: 0.3px solid gray;"></td>
+            </tr>
+            <tr style="font-size: 12px;">
+                <td width="50%" height="10"></td>
+                <td width="20%" colspan="2" align="left">
+                    <strong>TOTAL PENJUALAN</strong>
+                </td>
+                <td width="20%" align="right">
+                    Rp.300.000
+                </td>
+            </tr>
+            <tr>
+                <td height="10" style="border-bottom: 0.5px solid gray;" colspan="4"></td>
+            </tr>
+        </table>
+        <table width="100%" style="margin-top: 20px">
+            <tr>
+                <td width="60%">
+                    <small>
+                        Kurir:
+                        <br>
+                        <strong>
+                            SiCepat - Regular Package
+                        </strong>
+                    </small>
+                </td>
+                <td></td>
+            </tr>
+            <tr><td height="50"></td></tr>
+            <tr>
+                <td width="60%">
+                    <small>
+                        Invoice ini sah dan diproses oleh komputer
+                        <br>
+                        Silakan hubungi <strong style="color: red;">TAKATIKITUKU Care</strong> apabila kamu membutuhkan bantuan.
+                    </small>
+                </td>
+                <td align="right" style="vertical-align: bottom;">
+                    <small>
+                        <i>Terakhir diupdate: 03 Juni 2023 15:21 WIB</i>
+                    </small>
+                </td>
+            </tr>
+        </table>
+    </div>
+</page>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
+Route::get('/invoice', function(){
+    return view('invoice');
+});
 
 Route::middleware('guest')->group(function()
 {
